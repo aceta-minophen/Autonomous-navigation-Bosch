10e9b62d3b360ebdd0104fa1abb00c014b6a4246
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2022-10-01 13:01:28 +0530

    change file name

--- car.py
+++ car.py
@@ -3,7 +3,8 @@ from shapes import Paper, Triangle, Rectangle, Oval
 # Center
 a = 300
 b = 300
-
+c = 300
+d = 250
 # Colors
 sensorColor = "light coral"
 
@@ -13,6 +14,8 @@ def Car():
     paper = Paper()
 
     body = Rectangle()
+    bodyBlind1 = Rectangle()
+    bodyBlind2 = Rectangle()
     cam = Triangle()
     s1 = Triangle()
     s2 = Triangle()
@@ -20,19 +23,22 @@ def Car():
     s4 = Triangle()
 
     body.set_param(100, 200, a, b, "blue")
-
+    bodyBlind1.set_param(a-250, b-180, a-90, b, "red")
+    bodyBlind2.set_param(a-250, b-180, a+90, b, "red")
     # rect1.set_x(0)
     # rect1.set_y(0)
 
-    #circ1 = Oval()
+    circ1 = Oval()
 
-    #circ1.set_param(100, 100, a, b, "black")
+    circ1.set_param(20, 20, c, d, "black")
 
     # trian.draw()
 
     body.draw()
+    bodyBlind1.draw()
+    bodyBlind2.draw()
 
-    # circ1.draw()
+    circ1.draw()
 
     cam.draw(a, b-75, a-50, b-220, a+50, b-220, "light green")
     s1.draw(a-50, b-100, a-50, b-200, a-150, b-100, sensorColor)
@@ -43,4 +49,7 @@ def Car():
     paper.display()
 
 
+if(((185 <= c <= 235) and (240 <= d < 360)) or ((365 <= c <= 415) and (240 <= d < 360))):
+    {
+        print("blind spot")}
 Car()
